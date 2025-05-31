ad2ceaffa47c8ef224a99d4518e3db169a83aa29
tomaszkula <tomkul.priv@gmail.com>
2025-05-31 15:10:10 +0200

    Added elevator

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float speed = 1f;
+
+    [Header("References")]
+    [SerializeField] private Transform startPosition = null;
+    [SerializeField] private Transform endPosition = null;
+    [SerializeField] private Transform elevatorPlatform = null;
+
+    private Vector3 startPos = Vector3.zero;
+    private Vector3 endPos = Vector3.zero;
+
+    private Rigidbody2D rigidbody = null;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        startPos = startPosition.position;
+        endPos = endPosition.position;
+    }
+
+    private void Start()
+    {
+        elevatorPlatform.position = startPos;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPosition = Vector3.Lerp(startPos, endPos, Mathf.PingPong(Time.time * speed, 1f));
+        rigidbody.MovePosition(targetPosition);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(startPosition.position, endPosition.position);
+        Gizmos.DrawSphere(startPosition.position, 0.1f);
+        Gizmos.DrawSphere(endPosition.position, 0.1f);
+    }
+}
--- Assets/Scripts/MoveDefault.cs
+++ Assets/Scripts/MoveDefault.cs
@@ -6,6 +6,8 @@ public class MoveDefault : MonoBehaviour, IMove
     [Header("Settings")]
     [SerializeField] private float speed = 5f;
 
+    private Vector2 moveDirection = Vector2.zero;
+
     private Rigidbody2D rigidbody = null;
 
     public event Action<Vector2> OnMove = null;
@@ -15,13 +17,23 @@ public class MoveDefault : MonoBehaviour, IMove
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
     public void Move(Vector2 direction)
     {
-        Vector2 velocity = direction * speed;
+        moveDirection = direction;
+    }
+
+    private void ApplyMovement()
+    {
+        Vector2 velocity = moveDirection * speed;
         velocity.y = rigidbody.linearVelocity.y;
 
         rigidbody.linearVelocity = velocity;
 
-        OnMove?.Invoke(direction);
+        OnMove?.Invoke(moveDirection);
     }
 }
