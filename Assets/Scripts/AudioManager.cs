7e4fe4bd602d47d3acec54134f9e32ef9f816029
tomaszkula <tomkul.priv@gmail.com>
2025-05-31 13:58:40 +0200

    Added music and sound volume to settings
    Added default music

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using static UnityEngine.Rendering.DebugUI;
 
 public class AudioManager : BaseManager
 {
     [Header("Settings")]
+    [SerializeField] private AudioClip musicClip = null;
     [SerializeField] private AudioClip buttonClickSound = null;
 
     [Header("References")]
     [SerializeField] private AudioMixer audioMixer = null;
-    [SerializeField] private AudioSource audioSource = null;
+    [SerializeField] private AudioSource musicAudioSource = null;
+    [SerializeField] private AudioSource soundAudioSource = null;
 
     private const string AUDIO_MIXER_VOLUME_KEY = "Volume";
+    private const string AUDIO_MIXER_MUSIC_VOLUME_KEY = "MusicVolume";
+    private const string AUDIO_MIXER_SOUND_VOLUME_KEY = "SoundVolume";
 
     private const string IS_MUTED_KEY = "IsMuted";
+    private const string MUSIC_VOLUME_KEY = "MusicVolume";
+    private const string SOUND_VOLUME_KEY = "SoundVolume";
     public bool IsMuted
     {
         get => PlayerPrefs.GetInt(IS_MUTED_KEY, 0) == 1;
@@ -23,15 +30,57 @@ public class AudioManager : BaseManager
         }
     }
 
+    public int MusicVolume
+    {
+        get => PlayerPrefs.GetInt(MUSIC_VOLUME_KEY, 50);
+        set 
+        {
+            PlayerPrefs.SetInt(MUSIC_VOLUME_KEY, value);
+            audioMixer.SetFloat(AUDIO_MIXER_MUSIC_VOLUME_KEY, LinearToDecibel(value, 0, 100));
+        }
+    }
+
+    public int SoundVolume
+    {
+        get => PlayerPrefs.GetInt(SOUND_VOLUME_KEY, 50);
+        set 
+        {
+            PlayerPrefs.SetInt(SOUND_VOLUME_KEY, value);
+            audioMixer.SetFloat(AUDIO_MIXER_SOUND_VOLUME_KEY, LinearToDecibel(value, 0, 100));
+        }
+    }
+
     public override void Initialize()
     {
         audioMixer.SetFloat(AUDIO_MIXER_VOLUME_KEY, IsMuted ? -80f : 0f);
+        audioMixer.SetFloat(AUDIO_MIXER_MUSIC_VOLUME_KEY, LinearToDecibel(MusicVolume, 0, 100));
+        audioMixer.SetFloat(AUDIO_MIXER_SOUND_VOLUME_KEY, LinearToDecibel(SoundVolume, 0, 100));
+
+        PlayMusic();
 
         base.Initialize();
     }
 
+    public void PlayMusic()
+    {
+        musicAudioSource.clip = musicClip;
+        musicAudioSource.Play();
+    }
+
     public void PlayButtonClickSound()
     {
-        audioSource.PlayOneShot(buttonClickSound);
+        soundAudioSource.PlayOneShot(buttonClickSound);
     }
+
+    public static float LinearToDecibel(float value, float min, float max)
+    {
+        value = Mathf.Clamp(value, min, max);
+        value = Mathf.Lerp(0, 1, (value - min) / (max - min));
+
+        if (value <= 0.0001f)
+            return -80f;
+
+        return 20f * Mathf.Log10(value);
+    }
+
 }
--- Assets/Scripts/PopupSettings.cs
+++ Assets/Scripts/PopupSettings.cs
@@ -1,9 +1,14 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PopupSettings : Popup
 {
     [Header("Settings Popup")]
+    [SerializeField] private Slider musicVolumeSlider = null;
+    [SerializeField] private TextMeshProUGUI musicVolumeValueTMP = null;
+    [SerializeField] private Slider soundVolumeSlider = null;
+    [SerializeField] private TextMeshProUGUI soundVolumeValueTMP = null;
     [SerializeField] private Button mainMenuButton = null;
     [SerializeField] private Button quitButton = null;
 
@@ -11,6 +16,8 @@ public class PopupSettings : Popup
     {
         base.OnEnable();
 
+        musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeSliderChanged);
+        soundVolumeSlider.onValueChanged.AddListener(OnSoundVolumeSliderChanged);
         mainMenuButton.onClick.AddListener(OnMainMenuButtonClicked);
         quitButton.onClick.AddListener(OnQuitButtonClicked);
     }
@@ -19,8 +26,41 @@ public class PopupSettings : Popup
     {
         base.OnDisable();
 
+        musicVolumeSlider.onValueChanged.RemoveListener(OnMusicVolumeSliderChanged);
+        soundVolumeSlider.onValueChanged.RemoveListener(OnSoundVolumeSliderChanged);
         mainMenuButton.onClick.RemoveListener(OnMainMenuButtonClicked);
         quitButton.onClick.RemoveListener(OnQuitButtonClicked);
+    }
+
+    public override void Show()
+    {
+        base.Show();
+
+        musicVolumeSlider.wholeNumbers = true;
+        musicVolumeSlider.maxValue = 100;
+        musicVolumeSlider.minValue = 0;
+        musicVolumeSlider.value = ManagersController.Instance.GetManager<AudioManager>().MusicVolume;
+
+        soundVolumeSlider.wholeNumbers = true;
+        soundVolumeSlider.maxValue = 100;
+        soundVolumeSlider.minValue = 0;
+        soundVolumeSlider.value = ManagersController.Instance.GetManager<AudioManager>().SoundVolume;
+    }
+
+    private void OnMusicVolumeSliderChanged(float value)
+    {
+        Debug.Log($"Music Volume Changed: {value}");
+
+        ManagersController.Instance.GetManager<AudioManager>().MusicVolume = (int)value;
+        musicVolumeValueTMP.text = $"{value}%";
+    }
+
+    private void OnSoundVolumeSliderChanged(float value)
+    {
+        Debug.Log($"Sound Volume Changed: {value}");
+
+        ManagersController.Instance.GetManager<AudioManager>().SoundVolume = (int)value;
+        soundVolumeValueTMP.text = $"{value}%";
     }
 
     private void OnMainMenuButtonClicked()
