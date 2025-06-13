1a4db851cb625b80847cf7e2eabd8a40d1154e30
tomaszkula <tomkul.priv@gmail.com>
2025-06-13 11:27:39 +0200

    Added quit button to main menu screen

--- Assets/Scripts/Screens/MainMenuScreenUI.cs
+++ Assets/Scripts/Screens/MainMenuScreenUI.cs
@@ -6,23 +6,24 @@ public class MainMenuScreenUI : BaseScreen
     [Header("References")]
     [SerializeField] private Button playButton = null;
     [SerializeField] private Button selectLevelButton = null;
+    [SerializeField] private Button quitButton = null;
 
     private void OnEnable()
     {
         playButton.onClick.AddListener(OnPlayButtonClicked);
         selectLevelButton.onClick.AddListener(OnSelectLevelButtonClicked);
+        quitButton.onClick.AddListener(OnQuitButtonClick);
     }
 
     private void OnDisable()
     {
         playButton.onClick.RemoveListener(OnPlayButtonClicked);
         selectLevelButton.onClick.RemoveListener(OnSelectLevelButtonClicked);
+        quitButton.onClick.RemoveListener(OnQuitButtonClick);
     }
 
     private async void OnPlayButtonClicked()
     {
-        Debug.Log("Play button clicked! Starting game...");
-
         Manager.Instance.GetManager<AudioManager>().PlayButtonClickSound();
 
         await Manager.Instance.GetManager<LevelsManager>().Load();
@@ -30,10 +31,15 @@ public class MainMenuScreenUI : BaseScreen
 
     private void OnSelectLevelButtonClicked()
     {
-        Debug.Log("Select Level button clicked! Opening level selection...");
-
         Manager.Instance.GetManager<AudioManager>().PlayButtonClickSound();
 
         mainMenuUI.SelectScreen(MainMenuUI.ScreenType.LevelSelect);
+    }
+
+    private void OnQuitButtonClick()
+    {
+        Manager.Instance.GetManager<AudioManager>().PlayButtonClickSound();
+
+        Application.Quit();
     }
 }
